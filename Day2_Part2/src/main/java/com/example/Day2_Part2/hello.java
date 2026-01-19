2fc7fc7bc3e002ef98775ce4a58942bf09df6a1f
Yuvraj <yuvivishu17@gmail.com>
2026-01-19 15:10:17 +0530

    First API Created

--- Day2_Part2/src/main/java/com/example/Day2_Part2/hello.java
+++ Day2_Part2/src/main/java/com/example/Day2_Part2/hello.java
@@ -1,5 +1,6 @@
 package com.example.Day2_Part2;
 
+import com.example.Day2_Part2.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class hello {
 
     @GetMapping("/")
-    public String home(){
-        return "Hello ";
+    public StudentModel getStudent(){
+        StudentModel student = new StudentModel(18,"Virat","vk@gmail.com");
+        return student;
     }
 }
